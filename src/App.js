cb05fdd98f2f803792939aa8f54f6f9ca6286c28
yamanlutfi <it.yamanlutfi@gmail.com>
2019-03-22 05:08:24 +0700

    Bugfixes Bootstrap Javascript error

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './views/layout/Header';
 import Content from './views/layout/Content';
 import Footer from './views/layout/Footer';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 class App extends Component {
   render() {
